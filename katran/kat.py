a0ea89dcb1a492e3f476e6dcdd2c7183debd4eda
Sudev Suresh Sreedevi <54617167+GameGodS3@users.noreply.github.com>
2019-10-13 21:13:37 +0530

    Update kat.py
    Involving some GUI in the script

--- katran/kat.py
+++ katran/kat.py
@@ -9,10 +9,11 @@ Created on Fri Oct  4 23:12:15 2019
 import random
 import time
 from playsound import playsound
-import os
-def katran(path_to_audio):
+from tkinter import *
+from tkinter import messagebox
+def katran():
     
-    kat_sounds =os.listdir(path_to_audio)
+    kat_sounds = ["cat1.mp3", "cat2.mp3", "cat3.mp3", "cat4.mp3", "cat5.mp3", "cat6.mp3"]
     
     print('cat is running through ya keyboard.Press ctrl+c two times to exit')
       
@@ -25,7 +26,7 @@ def katran(path_to_audio):
         try:
             if i % 5 == 0:
                 r = random.uniform(0.01,0.2)
-                playsound(path_to_audio + random.choice(kat_sounds))
+                playsound("cataudios/" + random.choice(kat_sounds))
             a = random.randint(97, 136)
             print(chr(a), end="")        
             time.sleep(r)
@@ -36,12 +37,36 @@ def katran(path_to_audio):
             break
         
 
+window=Tk()
+window.eval('tk::PlaceWindow %s center' % window.winfo_toplevel())
 
-def main(path):
-    consent=input("Do you wanna let your cat run on your keyboard? say yes or no \n")
-    if consent=="yes":
-        print("Well then, let's begin")
-        katran(path)
-    else:
+#Defining a kitty pic
+box=Canvas(window, width=220, height=220)
+box.pack()
+kitty=PhotoImage(file='[insert directory path here]\\kat_ran\\kat.gif')        
+box.create_image(0, 0, anchor=NW, image=kitty)
+
+consentbox=messagebox.askyesno('Hey puss!', 'Do you wanna let your cat run on your keyboard?')
+
+if consentbox==True:
+    katran()
+    window.withdraw()
+    window.eval('tk::PlaceWindow %s center' % window.winfo_toplevel())
+    stopbox=messagebox.askokcancel('Calm Down Kitty', 'Press OK to take your cat off the keyboard')
+
+    window.deiconify()
+    window.destroy()
+    window.quit()
+
+    if stopbox==True:
         exit()
+else:
+    window.withdraw()
+    exit()
+
+window.deiconify()
+window.destroy()
+window.quit()
+
+
 
